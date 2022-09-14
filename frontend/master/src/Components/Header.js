2ee98b523273cbb2cc0f1e231e8b846613efa697
Kritt07 <dum98415@gmail.com>
2022-09-14 22:36:50 +0500

    animation and adaptive for header block

--- frontend/master/src/Components/Header.js
+++ frontend/master/src/Components/Header.js
@@ -5,7 +5,10 @@ import {BsStopCircle} from 'react-icons/bs'
 
 function Header() {
   const [play, setPlay] = useState(false)
+  // const [scroll, setScroll] = useState(0)
+  // const [classTitle, setClassTitle] = useState('header__title')
   const vidRef = useRef(null);
+
 
   const clickHolder = () => {
     setPlay(!play);
@@ -16,11 +19,30 @@ function Header() {
       vidRef.current.pause();
     }
   }
+
+
+  // const handleScroll = () => {
+  //   setScroll(window.scrollY)
+  // }
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll)
+  //   window.addEventListener('scroll', handleTitle)
+  //   console.log(scroll);
+  // })
+  // const handleTitle = () => {
+  //   if(scroll >= 0) {
+  //     setClassTitle('header__title scroll')
+  //   } else {
+  //     setClassTitle('header__title')
+  //   }
+  // }
   return (
     <div className='header'>
         <video ref={vidRef} className='header__video' src={video} type='video/webm' muted loop></video>
         <div className='header__box'>
-          <div className='header__title'>There is no other platforms for you as like</div>
+          <div className='header__title'>
+            There is no other platforms for you as like
+          </div>
           {play ? <BsStopCircle onClick={clickHolder} className='header__player' /> : <AiFillPlayCircle onClick={clickHolder} className='header__player' />}
           <div className='header__bottom'>
               <button className='header__btn btn'><a href='/#'>Try now</a></button>
