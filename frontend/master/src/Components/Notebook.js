40c5146ba69810858882249655b07955239475a9
Kritt07 <dum98415@gmail.com>
2022-09-27 22:34:07 +0500

    add animation in Note block and he is done!

--- frontend/master/src/Components/Notebook.js
+++ frontend/master/src/Components/Notebook.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
+import useScroll from '../CastomHooks/useScroll'
 import {IoIosArrowBack} from 'react-icons/io'
 import {MdOutlineNavigateNext} from 'react-icons/md'
 
@@ -32,11 +33,12 @@ function Notebook() {
     item4.current.style.transform = `translate(${-position * 100}%)`
     item5.current.style.transform = `translate(${-position * 100}%)`
   })
+  console.log(window.scrollY);
 
   return (
     <div className='note'>
         <div className='note__book'>
-          <div className='note__window'>
+          <div className={useScroll(1400) ? 'note__window active' : 'note__window'}>
             <button onClick={backHulder} name='back' className='note__button back'><IoIosArrowBack /></button>
             <div ref={item1} className='note__item i1'></div>
             <div ref={item2} className='note__item i2'></div>
@@ -48,8 +50,8 @@ function Notebook() {
         </div>
         <div className='note__text'>
             <h1 className='note__h1 h1'>NEW DESIGN</h1>
-            <h2 className='note__h2 h2'>Resposive design, just need your tap <b className='toch'>....</b></h2>
-            <p className='note__p p'>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn. Vestibulum felis euismod semper.</p>
+            <h2 className={useScroll(1200) ? 'note__h2 h2 active' : 'note__h2 h2'}>Resposive design, just need your tap <b className='toch'>....</b></h2>
+            <p className={useScroll(1300) ? 'note__p p active' : 'note__p p'}>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn. Vestibulum felis euismod semper.</p>
         </div>
     </div>
   )
